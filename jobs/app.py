c47c13aac2ccc32cf2e4314630e0bce9cc65796b
Pierre <pierre.fortes@gmail.com>
2020-10-31 22:11:02 +0100

    First commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
+
+def open_connection:
+
 
 @app.route('/')
 @app.route('/jobs')
