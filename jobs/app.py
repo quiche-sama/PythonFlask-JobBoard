7af1caf5109897a228d8121c0c5b1369c817f8fd
to155756 <Davidian09>
2020-12-08 13:35:29 +0100

    DB Connection

--- jobs/app.py
+++ jobs/app.py
@@ -1,17 +1,40 @@
 from flask import Flask, render_template, g
 import sqlite3
+
 PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
-def open_connection:
+
+def open_connection():
+    connection = getattr(g, '_database', None)
+    if connection is None:
+        connection = g._database = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    return results
 
 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     return render_template('index.html')
-
 
 
 if __name__ == "__main__":
