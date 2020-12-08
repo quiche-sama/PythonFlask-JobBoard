c746673aa7c2523e74f7e642343fbeead952f971
to155756 <Davidian09>
2020-12-08 13:40:12 +0100

    DB Connection

--- jobs/app.py
+++ jobs/app.py
@@ -28,7 +28,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-    close_connection()
+    cursor.close()
     return results
 
 
