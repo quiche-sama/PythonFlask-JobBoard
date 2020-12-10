09e5711f8e6931305cc231012aa2e3c11e714327
TO155756 <pierre.fortes@gmail.com>
2020-12-10 18:36:48 +0100

    Miss refactor

--- jobs/app.py
+++ jobs/app.py
@@ -44,8 +44,8 @@ def jobs():
 
 @app.route('/job/<job_id>')
 def job(job_id):
-    query = 'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ' + job_id
-    job = execute_sql(query, single=True)
+    query = 'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?'
+    job = execute_sql(query, values=[job_id], single=True)
     return render_template("job.html", job=job)
 
 
