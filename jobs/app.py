b25934ba3eb55968b2b72ee0bcccf7f93c25cc66
TO155756 <pierre.fortes@gmail.com>
2020-12-10 19:38:39 +0100

    Miss refactor

--- jobs/app.py
+++ jobs/app.py
@@ -64,9 +64,7 @@ def review(employer_id):
         rating = request.form['rating']
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
-        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
-                    values=(review, rating, title, date, status, employer_id),
-                    commit=True)
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
         redirect(url_for('employer', employer_id=employer_id))
 
     return render_template("review.html", employer_id=employer_id)
